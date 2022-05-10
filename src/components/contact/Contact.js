90df2a31a1d6483b30b7a7fd3b94014911ec6374
Priyanka <original.manaria@gmail.com>
2022-05-10 11:11:09 +0530

    reset contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,23 +1,36 @@
 import React from "react";
 import { social } from "../../data";
 import "./contact.css";
+import { Input, TextArea } from "semantic-ui-react";
+import Swal from "sweetalert2";
 import emailjs from "emailjs-com";
+const SERVICE_ID = "service_ualv7ad";
+const TEMPLATE_ID = "template_elaym4n";
+const USER_ID = "aL5ULp7irASKMkzpl";
 
 const Contact = () => {
-  const sendEmail = (e) => {
+  const handleOnSubmit = (e) => {
     e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_ualv7ad",
-        "template_elaym4n",
-        e.target,
-        "aL5ULp7irASKMkzpl"
-      )
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Message Sent Successfully",
+        });
+        e.target.reset();
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Ooops, something went wrong",
+          text: error.text,
+        });
+        e.target.reset();
+      }
+    );
   };
-
-  const handleSubmit = () => {};
 
   return (
     <main className="contact-section sec-padding" id="contact">
@@ -29,10 +42,11 @@ const Contact = () => {
         </div>
         <div className="row">
           <div className="contact-form">
-            <form onSubmit={sendEmail}>
+            <form onSubmit={handleOnSubmit}>
               <div className="row">
                 <div className="input-group">
                   <input
+                    control={Input}
                     name="name"
                     type="text"
                     placeholder="Name"
@@ -42,6 +56,7 @@ const Contact = () => {
                 </div>
                 <div className="input-group">
                   <input
+                    control={Input}
                     type="text"
                     name="user_email"
                     placeholder="Email"
@@ -51,6 +66,7 @@ const Contact = () => {
                 </div>
                 <div className="input-group">
                   <input
+                    control={Input}
                     name="subject"
                     type="text"
                     placeholder="Subject"
@@ -60,6 +76,7 @@ const Contact = () => {
                 </div>
                 <div className="input-group">
                   <textarea
+                    control={TextArea}
                     name="message"
                     placeholder="Message"
                     className="input-control"
